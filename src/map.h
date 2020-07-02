e4f57c06d523c8885668d6cd2d6e56724c05501b
Jacob Ososke <jmososke@gmail.com>
2020-07-02 15:21:22 -0700

    Initializing particles with a normal distribution

--- src/map.h
+++ src/map.h
@@ -1,10 +1,3 @@
-/**
- * map.h
- *
- * Created on: Dec 12, 2016
- * Author: mufferm
- */
-
 #ifndef MAP_H_
 #define MAP_H_
 
--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -1,12 +1,4 @@
-/**
- * particle_filter.cpp
- *
- * Created on: Dec 12, 2016
- * Author: Tiffany Huang
- */
-
 #include "particle_filter.h"
-
 #include <math.h>
 #include <algorithm>
 #include <iostream>
@@ -15,11 +7,11 @@
 #include <random>
 #include <string>
 #include <vector>
-
 #include "helper_functions.h"
 
 using std::string;
 using std::vector;
+using std::normal_distribution;
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
   /**
@@ -30,8 +22,34 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 0;  // TODO: Set the number of particles
+  num_particles = 100;  // TODO: Set the number of particles
 
+  if (is_initialized) return;
+
+  //Sample from these normal distributions like this: 
+  //   sample_x = dist_x(gen);
+  //   where "gen" is the random engine initialized here
+  std::default_random_engine gen;
+
+  // This line creates a normal (Gaussian) distribution for x
+  normal_distribution<double> dist_x(x, std[0]);
+  // This line creates a normal (Gaussian) distribution for y
+  normal_distribution<double> dist_y(y, std[1]);
+  // This line creates a normal (Gaussian) distribution for theta
+  normal_distribution<double> dist_theta(theta, std[2]);
+
+  for (int i = 0; i < num_particles; i++)
+  {
+    Particle p;
+    p.id = i;
+    p.x = dist_x(gen);
+    p.y = dist_y(gen);
+    p.theta = dist_theta(gen);
+    p.weight = 1.0;
+    particles.push_back(p);
+  }
+  // The filter is now initialized.
+  is_initialized = true;
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], 
