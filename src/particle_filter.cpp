48a90be4975cb3f712c612ec28fc8e4e73d54c7c
Jacob Ososke <jmososke@gmail.com>
2020-07-02 18:10:30 -0700

    Fixing gaussian filter bug

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -9,7 +9,7 @@
 #include <vector>
 #include <float.h>
 #include "helper_functions.h"
-//#include "multiv_gauss.h"
+#include "multiv_gauss.h"
 
 using std::string;
 using std::vector;
@@ -19,24 +19,6 @@ using std::normal_distribution;
   //   sample_x = dist_x(gen);
   //   where "gen" is the random engine initialized here
   std::default_random_engine gen;
-
-double multiv_prob(double sig_x, double sig_y, double x_obs, double y_obs,
-                   double mu_x, double mu_y) {
-  // calculate normalization term
-  double gauss_norm;
-  gauss_norm = 1 / (2 * M_PI * sig_x * sig_y);
-
-  // calculate exponent
-  double exponent;
-  exponent = (pow(x_obs - mu_x, 2) / (2 * pow(sig_x, 2)))
-               + (pow(y_obs - mu_y, 2) / (2 * pow(sig_y, 2)));
-    
-  // calculate weight using normalization terms and exponent
-  double weight;
-  weight = gauss_norm * exp(-exponent);
-    
-  return weight;
-}
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
   /**
